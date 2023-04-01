00ff5ae8d720ca5a6cc2bbaa184319cfeb9ab3df
Paulo <pshenrique2015@gmail.com>
2023-04-01 15:04:21 -0300

    Primeira commit.

---
+++ src/main/java/com/paulo/crudprodutos/enums/Categoria.java
@@ -0,0 +1,5 @@
+package com.paulo.crudprodutos.enums;
+
+public enum Categoria {
+    Alimento, Eletronico, Brinquedo, Ferramenta, Decoracao
+}
---
+++ src/main/java/com/paulo/crudprodutos/modelo/Produto.java
@@ -0,0 +1,84 @@
+package com.paulo.crudprodutos.modelo;
+
+import com.paulo.crudprodutos.enums.Categoria;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", length = 50, nullable = false)
+    private String nome;
+
+    @Column(name = "descricao", length = 200)
+    private String descricao;
+
+    @Column(name = "preco", nullable = false)
+    @Positive(message = "O valor do preço precisa ser positivo.")
+    private double preco;
+    
+    @Column(name = "categoria", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+    public Produto() {
+    }
+
+    public Produto(Long id, String nome, String descricao,
+            @Positive(message = "O valor do preço precisa ser positivo.") double preco, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+}
